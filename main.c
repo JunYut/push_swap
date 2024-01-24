9674c3163100a79075410cd436fcb27ec256ab30
Tan Jun Yu <tjun-yu@u91z05s05.42kl.edu.my>
2024-01-24 12:46:25 +0800

    'calc_range()' is not working as intended
    main.c: restructured tests

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tjun-yu <tjun-yu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 12:52:37 by tjun-yu           #+#    #+#             */
-/*   Updated: 2024/01/24 12:19:17 by tjun-yu          ###   ########.fr       */
+/*   Updated: 2024/01/24 12:45:06 by tjun-yu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,27 +22,56 @@
 // 	free_stack(A);
 // }
 
+
+int	TEST(char *test);
+
 /**
  * @test void push(t_stack *stack, int element)
  * @test void pop(t_stack *stack)
 */
 int main()
 {
-	console_log("PUSH TEST:");
-	console_log("__________________________________________");
+	char	*test = "PUSH";
 	t_stack *stack = new_stack();
-	for (int i = 1; i <= 10; i++)
+
+	switch (TEST(test))
 	{
-		push(stack, i);
-		print_stack(stack);	console_log("");
+	case 1:
+		console_log("PUSH TEST:");
+		console_log("__________________________________________");
+		for (int i = 1; i <= 10; i++)
+		{
+			push(stack, i);
+			print_stack(stack);	console_log("");
+		}
+		break;
+	case 2:
+		console_log("POP TEST:");
+		console_log("__________________________________________");
+		for (int i = 1; i <= 10; i++)
+		{
+			push(stack, i);
+			print_stack(stack);	console_log("");
+		}
+		for (int i = 0; i < 10; i++)
+		{
+			print_stack(stack);	console_log("");
+			pop(stack);
+		}
+		break;
+
+	default:
+		break;
 	}
 
-	console_log("POP TEST:");
-	console_log("__________________________________________");
-	for (int i = 0; i < 10; i++)
-	{
-		print_stack(stack);	console_log("");
-		pop(stack);
-	}
 	free_stack(stack);
+}
+
+int	TEST(char *test)
+{
+	if (same_str("PUSH", test))
+		return (1);
+	if (same_str("POP", test))
+		return (2);
+	return (0);
 }
