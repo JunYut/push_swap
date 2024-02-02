495a31eb0a8ecbad127d0b15c685772a469c9c79
Vanilla <v@apx-vso-pico.Orchid>
2024-02-02 06:42:27 +0000

    ft_atoi: now handles string representation of number that starts with '+'

--- src/parser/atoi.c
+++ src/parser/atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   atoi.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tjun-yu <tanjunyu8888@gmail.com>           +#+  +:+       +#+        */
+/*   By: v <v@student.42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 12:04:29 by tjun-yu           #+#    #+#             */
-/*   Updated: 2024/01/18 09:57:19 by tjun-yu          ###   ########.fr       */
+/*   Updated: 2024/02/02 14:40:46 by v                ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,10 @@ long	ft_atoi(const char *integer)
 	num = 0;
 	p_or_n = 1;
 	i = -1;
-	if (integer[0] == '-')
-	{
-		p_or_n = -1;
+	if (integer[0] == '+' || integer[0] == '-')
 		++i;
-	}
+	if (integer[0] == '-')
+		p_or_n = -1;
 	while (integer[++i] != 0)
 		num = num * 10 + integer[i] - '0';
 	return (num * p_or_n);
