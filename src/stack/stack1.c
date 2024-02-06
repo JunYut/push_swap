4970682d62f5d25f155fdb805a3a2b87acb5e31a
Tan Jun Yu <tjun-yu@u91z05s03.42kl.edu.my>
2024-02-06 11:34:24 +0800

    Now 'find' functions checks if the stack is empty before proceeding

--- src/stack/stack1.c
+++ src/stack/stack1.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   stack1.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: v <v@student.42.fr>                        +#+  +:+       +#+        */
+/*   By: tjun-yu <tjun-yu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 14:58:22 by tjun-yu           #+#    #+#             */
-/*   Updated: 2024/02/02 12:20:01 by v                ###   ########.fr       */
+/*   Updated: 2024/02/06 11:33:20 by tjun-yu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	find_negative(t_stack *stack)
 	t_node	*curr;
 	int		i;
 
+	if (stack->size == 0)
+		return (-1);
 	curr = stack->head;
 	i = 0;
 	if (curr->num < 0)
@@ -38,6 +40,8 @@ int	find_position(t_stack *stack, int target)
 	t_node	*curr;
 	int		i;
 
+	if (stack->size == 0)
+		return (-1);
 	curr = stack->head;
 	i = 0;
 	if (curr->num == target)
@@ -59,6 +63,8 @@ t_node	*find_max_node(t_stack *stack)
 	t_node	*max;
 	t_node	*curr;
 
+	if (stack->size == 0)
+		return (NULL);
 	curr = stack->head;
 	max = curr;
 	if (curr->num > max->num)
@@ -78,6 +84,8 @@ t_node	*find_min_node(t_stack *stack)
 	t_node	*min;
 	t_node	*curr;
 
+	if (stack->size == 0)
+		return (NULL);
 	curr = stack->head;
 	min = curr;
 	if (curr->num < min->num)
