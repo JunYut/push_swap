26b14ed7e5110187e25d5fb142746ef2869e79be
Tan Jun Yu <tanjunyu8888@gmail.com>
2024-02-23 14:24:03 +0800

    mid_sort: // steps: // +ve 5   stack: 51 -> 51 // +ve 10  stack: 87 -> 87 // +ve 100 stack: 1158 -> 1158 // +ve 500 stack: 14538 -> 14538 //--------------------------------------------------------- // sorted rate: // +ve 5   stack: 4/4 -> 4/4 // +ve 10  stack: 9/9 -> 9/9 // +ve 100 stack: 99/99 -> 99/99 // +ve 500 stack: 499/499 -> 499/499

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: we <we@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 12:52:37 by tjun-yu           #+#    #+#             */
-/*   Updated: 2024/02/23 14:05:33 by we               ###   ########.fr       */
+/*   Updated: 2024/02/23 14:12:02 by we               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ int	main(int argc, char *argv[])
 
 		// mid_sort_a0(A, B); mid_sort_b(B, A); select_sort(A, B);
 
-		mid_sort_a0(A, B); mid_sort_b(B, A);
+		mid_sort(A, B);
 
 		(void)size;
 	}
