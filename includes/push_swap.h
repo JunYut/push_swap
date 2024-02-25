23aa6e581c2de451a9e82fdbc40ca1922fef5860
Tan Jun Yu <tanjunyu8888@gmail.com>
2024-02-26 01:34:33 +0800

    TODO: implement compact_sort

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: we <we@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 12:51:39 by tjun-yu           #+#    #+#             */
-/*   Updated: 2024/02/26 00:07:29 by we               ###   ########.fr       */
+/*   Updated: 2024/02/26 01:33:41 by we               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ void	select_sort_B(t_stack *A, t_stack *B, int chunck);
 void	insert_sort(t_stack *A, t_stack *B);
 void	insert_sort1(t_stack *A, t_stack *B);
 void	bubble_sort(t_stack *A, t_stack *B);
+
+void	compact_sort(t_stack *A, t_stack *B);
 
 void	sort_stack_A(t_stack *A, t_stack *B);
 void	sort_stack_B(t_stack *B, t_stack *A);
